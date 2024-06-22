7467d39a00eae70a6f941c2fae8b03a911a7d761
TorresJona11 <alvarezmelvyn@gmail.com>
2024-06-22 15:02:59 -0600

    segundo commit de la lista to-do

--- app.py
+++ app.py
@@ -28,3 +28,41 @@ class TareaSchema(ma.SQLAlchemyAutoSchema):
 
 tarea_schema = TareaSchema()
 tareas_schema = TareaSchema(many=True)
+
+@app.route('/tareas', methods=['GET'])
+def obtener_todas_las_tareas():
+    try:
+        tareas = Tarea.query.all()
+        resultado = tareas_schema.dump(tareas)
+        return jsonify(resultado), 200
+    except Exception as e:
+        return jsonify({'mensaje': 'Error al obtener los datos', 'error': str(e)}), 500
+
+@app.route('/tareas/<int:id>', methods=['GET'])
+def obtener_tarea_id(id):
+    try:
+        tarea = Tarea.query.get(id)
+        if not tarea:
+            return jsonify({'mensaje': 'Tarea no encontrada'}), 404
+        return tarea_schema.jsonify(tarea), 200
+    except Exception as e:
+        return jsonify({'mensaje': 'Error al obtener la tarea', 'error': str(e)}), 500
+
+@app.route('/tareas', methods=['POST'])
+def agregar_tarea():
+    try:
+        descripcion = request.json['descripcion']
+        fecha_vencimiento_str = request.json['fecha_vencimiento']
+        
+        fecha_vencimiento = datetime.strptime(fecha_vencimiento_str, '%d-%m-%Y').date()
+        
+        nueva_tarea = Tarea(descripcion=descripcion, fecha_vencimiento=fecha_vencimiento)
+        basedatos.session.add(nueva_tarea)
+        basedatos.session.commit()
+        return tarea_schema.jsonify(nueva_tarea), 201
+    except ValueError:
+        basedatos.session.rollback()
+        return jsonify({'mensaje': 'Formato de fecha incorrecto. Debe ser DD-MM-YYYY.'}), 400
+    except Exception as e:
+        basedatos.session.rollback()
+        return jsonify({'mensaje': 'Error al agregar la tarea', 'error': str(e)}), 500
